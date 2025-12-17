ef329def4cc4a69a33e3977d1b36defc8ff2b93f
yulieth07 <yulieth_junco@soy.sena.edu.co>
2025-12-16 22:32:21 -0500

    Add files via upload

---
+++ alquiler/editar.php
@@ -0,0 +1,21 @@
+<?php
+include("../config/conexion.php");
+$id = $_GET['id'];
+$res = $conn->query("SELECT * FROM alquiler WHERE id_alquiler=$id");
+$a = $res->fetch_assoc();
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+<h2>Editar Alquiler</h2>
+
+<form method="post" action="actualizar.php">
+<input type="hidden" name="id" value="<?= $a['id_alquiler'] ?>">
+<input type="date" name="fecha" value="<?= $a['fecha'] ?>">
+<input name="horas" value="<?= $a['horas'] ?>">
+<input name="precio" value="<?= $a['precio'] ?>">
+<button>Actualizar</button>
+</form>
+</body>
+</html>
