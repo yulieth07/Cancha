8d28ed18b789af502dd16404b8e2baaf3064bbe8
yulieth07 <yulieth_junco@soy.sena.edu.co>
2025-12-17 00:00:46 -0500

    Refactor include statement to use include_once

--- factura/actualizar.php
+++ factura/actualizar.php
@@ -1,8 +1,9 @@
 <?php
-include("../config/conexion.php");
+include_once("../config/conexion.php");
 
 $stmt = $conn->prepare("CALL sp_factura_update(?,?)");
 $stmt->bind_param("is", $_POST['id'], $_POST['estado']);
 $stmt->execute();
 
 header("Location: listar.php");
+
