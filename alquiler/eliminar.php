34b7b6ecfab5456b26a3ae77399c076cf2ee8813
yulieth07 <yulieth_junco@soy.sena.edu.co>
2025-12-16 23:59:51 -0500

    Refactor include statement to include_once

--- alquiler/eliminar.php
+++ alquiler/eliminar.php
@@ -1,8 +1,9 @@
 <?php
-include("../config/conexion.php");
+include_once("../config/conexion.php");
 
 $stmt = $conn->prepare("CALL sp_alquiler_delete(?)");
 $stmt->bind_param("i", $_GET['id']);
 $stmt->execute();
 
 header("Location: listar.php");
+
