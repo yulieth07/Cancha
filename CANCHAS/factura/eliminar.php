5a78452a4183353134a3187eafcfa5cbaa26cb3b
yulieth07 <yulieth_junco@soy.sena.edu.co>
2025-12-16 22:34:23 -0500

    Delete CANCHAS directory

--- CANCHAS/factura/eliminar.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("../config/conexion.php");
-
-$stmt = $conn->prepare("CALL sp_factura_delete(?)");
-$stmt->bind_param("i", $_GET['id']);
-$stmt->execute();
-
-header("Location: listar.php");
